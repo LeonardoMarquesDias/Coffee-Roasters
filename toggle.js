1e5e3ef5bbeaa4154f306530d0f8f40e3f4b7716
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-03-17 23:15:37 +0000

    menu toggle

---
+++ toggle.js
@@ -0,0 +1,7 @@
+
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+  alert("Hello! I am an alert box!!")
+});
